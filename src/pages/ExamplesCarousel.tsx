ab20afb73d4670019b8c2c414218dbceee39c83b
Nemanja Gajic <nemanja@MacBook-Pro-5.local>
2023-09-24 20:26:07 +0200

    removed unused imports

--- src/pages/ExamplesCarousel.tsx
+++ src/pages/ExamplesCarousel.tsx
@@ -1,6 +1,5 @@
-import React, { useRef, useState } from 'react'
+import React, { useState } from 'react'
 import ExampleItem from '../components/examples/ExampleItem'
-import Container from '../components/shared/Container'
 import Slider from 'react-slick'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
