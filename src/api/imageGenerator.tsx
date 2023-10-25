35deeb3299a92ca5900208b4a51d3cdb0a716694
Nemanja Gajic <nemanja@MacBook-Pro-5.local>
2023-10-25 20:12:09 +0200

    removed calling the next leg api directly

--- src/api/imageGenerator.tsx
+++ src/api/imageGenerator.tsx
@@ -1,11 +1,9 @@
 import axios from 'axios'
 
 export const generateImage = async (description: string, ref: string) => {
-  const token = process.env.REACT_APP_THE_NEXT_LEG_TOKEN
-  axios.defaults.headers.common = {'Authorization': `Bearer ${token}`}
-  const response = await axios.post('https://api.thenextleg.io', {
-    cmd: "imagine",
-    msg: description,
+  const baseUrl = process.env.REACT_APP_BASE_API_URL
+  const response = await axios.post(`${baseUrl}/generateImage`, {
+    description,
     ref
   })
 
