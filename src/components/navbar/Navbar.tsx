e0a244a21e689162ad8ef7025a67de89b552928e
Nemanja Gajic <nemanja.gajicru96@gmail.com>
2023-12-06 13:33:53 +0100

    added logos

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Container from '../shared/Container'
 import { BagOutline } from 'react-ionicons'
 import { scrollToSection } from '../../utils/pageNavigation'
+// @ts-ignore
+import logo from '../../assets/images/logo3.png'
 
 type NavbarTypes = {
   onCartClicked: () => void
@@ -9,14 +11,14 @@ type NavbarTypes = {
 
 const Navbar = ({ onCartClicked }: NavbarTypes) => (
   <Container>
-    <div className='flex flex-row w-full justify-between py-6'>
-      <div className='flex justify-center items-center cursor-pointer'>Logo</div>
+    <div className='flex flex-row w-full justify-between'>
+      <img src={logo} width={100} />
       <div className='flex flex-row hidden sm:inline-flex'>
         <div onClick={() => scrollToSection('examples')} className='flex justify-center items-center px-8 cursor-pointer'>Primeri</div>
         <div onClick={() => scrollToSection('tShirtSizes')} className='flex justify-center items-center px-8 cursor-pointer'>Veličine</div>
         <div onClick={() => scrollToSection('questions')} className='flex justify-center items-center px-8 cursor-pointer'>Pitanja</div>
       </div>
-      <div className='p-2 rounded-md shadow cursor-pointer'>
+      <div className='p-2 rounded-md shadow cursor-pointer my-8'>
         <BagOutline
           onClick={onCartClicked}
           height="25px"
