e8331a00bedc3ccf1dfe78be4582c4f94ff7e5ab
Nemanja Gajic <nemanja.gajicru96@gmail.com>
2023-12-13 19:54:31 +0100

    showing number in queue implemented

--- src/pages/landing/ClothesGenerator.tsx
+++ src/pages/landing/ClothesGenerator.tsx
@@ -7,6 +7,7 @@ import axios from "axios";
 
 const PROGRESS_BAR_FETCHING_INTERVAL_MS = 3000
 const DEFAULT_PROGRESS_INCREMENT = 2
+const QUEUE_FETCHING_INTERVAL_MS = 5000
 
 type ClothesGeneratorTypes = {
   userId: string
@@ -17,6 +18,7 @@ const ClothesGenerator = ({ userId }: ClothesGeneratorTypes) => {
   const [generatedImages, setGeneratedImages] = useState([])
   const [focusedPhotoIndex, setFocusedPhotoIndex] = useState(0)
   const [progressBarPercentage, setProgressBarPercentage] = useState(0)
+  const [myIndexInGenerationQueue, setMyIndexInGenerationQueue] = useState(null)
 
   useEffect(() => {
     const baseApiUrl = process.env.REACT_APP_BASE_API_URL|| ''
@@ -60,8 +62,18 @@ const ClothesGenerator = ({ userId }: ClothesGeneratorTypes) => {
     }
   }
 
+  const fetchGenerationQueueData = async () => {
+    try {
+      const { data: { imageRequestsQueue } } = await axios.get(`${process.env.REACT_APP_BASE_API_URL}/imageRequestsQueue`)
+      const myGenerationIndex = imageRequestsQueue.findIndex((ir: any) => ir.ref === userId)
+      setMyIndexInGenerationQueue(myGenerationIndex)
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
   useEffect(() => {
-    if (!isGeneratingImages) return
+    if (!isGeneratingImages || myIndexInGenerationQueue !== 0) return
     const interval = setInterval(() => {
       fetchAndUpdateProgress()
     }, PROGRESS_BAR_FETCHING_INTERVAL_MS)
@@ -74,11 +86,25 @@ const ClothesGenerator = ({ userId }: ClothesGeneratorTypes) => {
         clearInterval(interval)
       }
     }
-  }, [isGeneratingImages])
+  }, [isGeneratingImages, myIndexInGenerationQueue])
+
+  useEffect(() => {
+    if (!isGeneratingImages || myIndexInGenerationQueue === 0) return
+    const interval = setInterval(() => {
+      fetchGenerationQueueData()
+    }, QUEUE_FETCHING_INTERVAL_MS)
+
+    return () => {
+      if (interval) {
+        clearInterval(interval)
+      }
+    }
+  }, [isGeneratingImages, myIndexInGenerationQueue])
 
   const clearGeneratedImages = () => {
     setGeneratedImages([])
     setProgressBarPercentage(0)
+    setMyIndexInGenerationQueue(null)
   }
 
   const handleGenerateImage = () => {
@@ -145,8 +171,16 @@ const ClothesGenerator = ({ userId }: ClothesGeneratorTypes) => {
               <div className={`text-gray-400 font-bold flex justify-center items-center rounded-md h-full w-full relative overflow-hidden`}>
                 <div className={`w-[140px] h-[240px] ${isGeneratingImages ? gradientBgLoaderStyle : 'bg-gray-200'}`} />
                 {isGeneratingImages && (
-                  <div className="w-full bg-gray-200 h-2.5 dark:bg-gray-100 absolute bottom-0 rounded-b-md">
-                    <div className="bg-light-blue h-2.5 rounded-full" style={{ width: `${progressBarPercentage}%`, transition: 'width 0.3s ease' }}></div>
+                  <div className="w-full absolute bottom-0">
+                    {myIndexInGenerationQueue && myIndexInGenerationQueue > 0 ? (
+                    <div className="text-center py-1 text-sm">
+                      {`Trenutno ${myIndexInGenerationQueue} ljudi kreira svoju majicu`}
+                    </div>
+                    ) : (
+                      <div className="w-full bg-gray-200 h-2.5 dark:bg-gray-100 absolute bottom-0 rounded-b-md">
+                        <div className="bg-light-blue h-2.5 rounded-full" style={{ width: `${progressBarPercentage}%`, transition: 'width 0.3s ease' }}></div>
+                      </div>
+                    )}
                   </div>
                 )}
               </div>
