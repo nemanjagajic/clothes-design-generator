cdb01afebaadaae023e1e26d48aa9aae97ee8c1c
Nemanja Gajic <nemanja.gajicru96@gmail.com>
2023-12-13 11:27:27 +0100

    removed orders from dashboard

--- src/pages/dashboard/DashboardHome.tsx
+++ src/pages/dashboard/DashboardHome.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
-import Generations from "./Generations";
+import Generations from './Generations'
 enum Tabs {
-  ORDERS = 'ORDERS',
-  GENERATIONS = 'GENERATIONS',
+  GENERATIONS = 'GENERATIONS'
 }
 
 const DashboardHome = () => {
-  const [selectedTab, setSelectedTab] = React.useState<Tabs>(Tabs.ORDERS)
+  const [selectedTab, setSelectedTab] = React.useState<Tabs>(Tabs.GENERATIONS)
 
   const renderNavbar = () => (
     <div className='flex flex-row hidden sm:inline-flex py-6 border border-b w-full'>
-      <div onClick={() => setSelectedTab(Tabs.ORDERS)} className={`flex justify-center items-center px-8 cursor-pointer ${selectedTab === Tabs.ORDERS && 'text-light-blue'}`}>Narudžbine</div>
       <div onClick={() => setSelectedTab(Tabs.GENERATIONS)} className={`flex justify-center items-center px-8 cursor-pointer ${selectedTab === Tabs.GENERATIONS && 'text-light-blue'}`}>Generisanja</div>
     </div>
   )
