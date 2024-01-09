868827d9b22c2ad1296df56768b5f85ab336aa7b
Nemanja Gajic <nemanja.gajicru96@gmail.com>
2024-01-09 15:17:19 +0100

    scrolling to tshirt container and showing progress percentage

--- src/utils/pageNavigation.ts
+++ src/utils/pageNavigation.ts
@@ -4,3 +4,13 @@ export const scrollToSection = (sectionId: string) => {
     window.scrollTo({ top: sectionOffset - 120, behavior: 'smooth' })
   }
 }
+
+export const checkIfElementIsInViewPort = (element: HTMLElement) => {
+  const rect = element.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  )
+}
