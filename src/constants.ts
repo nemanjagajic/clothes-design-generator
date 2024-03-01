4e59c1c2091768e282ef8d52e507b54f29a5cf5b
Vladimir Antonic <vladimirantonic@Vladimirs-MacBook-Pro.local>
2024-03-01 16:02:05 +0100

    Color update

--- src/constants.ts
+++ src/constants.ts
@@ -2,9 +2,9 @@
 export const colorOptions = {
   white: 'bg-white',
   black: 'bg-black',
-  green: 'bg-green-500',
-  red: 'bg-red-500',
-  gray: 'bg-gray-500',
+  green: 'bg-[#595906]',
+  red: 'bg-[#b91010]',
+  gray: 'bg-[#3a3a39]',
 }
 
 export type ColorOption = keyof typeof colorOptions
