5a4d1def39f522ca665376ccec3cf5592234bb4a
nemanjagajic <nemanja.gajic@ananas.rs>
2024-03-02 20:16:56 +0100

    minor buttons fix

--- src/components/shared/ButtonNavigate.tsx
+++ src/components/shared/ButtonNavigate.tsx
@@ -6,24 +6,21 @@ type ButtonNavigateTypes = {
   onClick: () => void
 }
 const ButtonNavigate = ({ direction = "NEXT", onClick }: ButtonNavigateTypes) => {
-  const [isHovered, setIsHovered] = useState(false)
 
   return (
     <div
-      className={`flex items-center justify-center height-[42px] w-[110px] p-2 rounded-3xl border border-1 border-light-blue mx-2 hover:bg-light-blue transition-all ease-in cursor-pointer`}
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
+      className={`flex items-center justify-center height-[42px] w-[110px] p-2 rounded-3xl border border-1 border-light-blue mx-2 transition-all ease-in cursor-pointer outline-none`}
     >
       {direction === "NEXT" ? (
         <ChevronForward
-          color={isHovered ? 'white' : '#0090F8'}
+          color={'#0090F8'}
           height="30px"
           width="30px"
           onClick={onClick}
         />
       ) : (
         <ChevronBack
-          color={isHovered ? 'white' : '#0090F8'}
+          color={'#0090F8'}
           height="30px"
           width="30px"
           onClick={onClick}
